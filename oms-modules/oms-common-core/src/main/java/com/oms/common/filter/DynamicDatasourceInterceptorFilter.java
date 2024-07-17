16908b6e0e0177ef557ec864e3853578b3a00c0f
yueguang <yueguang@qingmutec.com>
2024-07-17 18:38:08 +0800

    OMS-子模块集合调整-Filter

--- oms-modules/oms-common-core/src/main/java/com/oms/common/filter/DynamicDatasourceInterceptorFilter.java
+++ oms-modules/oms-common-core/src/main/java/com/oms/common/filter/DynamicDatasourceInterceptorFilter.java
@@ -7,11 +7,10 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 @WebFilter(filterName = "DynamicDatasourceInterceptorFilter", value = {"/*"})
 public class DynamicDatasourceInterceptorFilter implements Filter {
-    @Value("${spring.datasource.dynamic.datasource.master.databaseName:false}")
-    private String databaseName;
 
     @Override
     public void init(FilterConfig filterConfig) {
@@ -23,14 +22,15 @@ public class DynamicDatasourceInterceptorFilter implements Filter {
         System.out.println("===> DynamicDatasourceInterceptor doFilter");
         HttpServletRequest req = (HttpServletRequest)servletRequest;
         HttpServletResponse resp = (HttpServletResponse)servletResponse;
-        databaseName = databaseName.toLowerCase();
-        System.out.println("databaseName:"+databaseName);
-
-        String dataKey ="qm_"+databaseName;
+        // 获取请求参数
+        String dataKey = req.getParameter("company_code");
         System.out.println("dataKey:"+dataKey);
         DynamicDataSourceContextHolder.clear();
-        //切换到对应poolName的数据源
-        DynamicDataSourceContextHolder.push(dataKey);
+        if (!Objects.isNull(dataKey)) {
+            System.out.println("切库dataKey:"+dataKey);
+            //切换到对应poolName的数据源
+            DynamicDataSourceContextHolder.push(dataKey);
+        }
 
         chain.doFilter(req,resp);
     }
